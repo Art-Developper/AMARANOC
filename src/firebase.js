06ffc66ed5819ce385abe8a829f047570aa11f8f
Artyom <artyom.santoyan.ait@gmail.com>
2025-10-29 03:17:14 -0700

    add database

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAm7sBinnEv36MUlDj4-O98S0x5JhjZZFQ",
+  authDomain: "amaranoc-a35ab.firebaseapp.com",
+  projectId: "amaranoc-a35ab",
+  storageBucket: "amaranoc-a35ab.firebasestorage.app",
+  messagingSenderId: "103884148399",
+  appId: "1:103884148399:web:497edd0c96366f50c1ce5a",
+  measurementId: "G-JNQJWCFMN2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/AMARANOC.git',
   plugins: [react()],
 })
